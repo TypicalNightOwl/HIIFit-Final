3f7a74557295e2b39eae3b7f2ea675c3343fa42b
Ryan Westhoelter <jwesth1@live.spcollege.edu>
2024-09-11 18:57:28 -0400

    Initial Commit

---
+++ HIIFit/HIIFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIIFitApp.swift
+//  HIIFit
+//
+//  Created by Ryan Westhoelter on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HIIFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
